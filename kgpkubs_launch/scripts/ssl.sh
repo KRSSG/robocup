311f9f84d05a43960687591103e87cd1f0685d99
americast <sayan.sinha@iitkgp.ac.in>
2017-12-09 19:31:48 +0530

    fix make issue

--- kgpkubs_launch/scripts/ssl.sh
+++ kgpkubs_launch/scripts/ssl.sh
@@ -28,9 +28,9 @@ fi
 
 launcher    "core"         "roscore"
 pr_launcher "vision"    "vision.launch"
-pr_launcher "belief_state"      "belief_state.launch"
-pr_launcher "grsim_comm"    "grsim_comm.launch"
-pr_launcher "robot"         "robot.launch"
+# pr_launcher "belief_state"      "belief_state.launch"
+# pr_launcher "grsim_comm"    "grsim_comm.launch"
+# pr_launcher "robot"         "robot.launch"
 #pr_launcher "bot_comm"         "bot_comm.launch"
 #pr_launcher "test_play_node" "test_play.launch"
 # pr_launcher "play_node" "play.launch"
