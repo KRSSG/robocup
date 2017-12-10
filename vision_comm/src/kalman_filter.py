2d517611a561885e842508fe17d5ef0002a72240
americast <sayan.sinha@iitkgp.ac.in>
2017-12-10 16:26:38 +0530

    Plot in kalman

--- vision_comm/src/kalman_filter.py
+++ vision_comm/src/kalman_filter.py
@@ -10,7 +10,12 @@ from std_msgs.msg import String
 from krssg_ssl_msgs.msg import BeliefState
 import time
 from copy import copy
+from math import *
+
+LAST_FRAMES = 5
+
 dict_old = []
+count1=0
 
 def filter(vel_set):
     n_timesteps = len(vel_set)
@@ -24,18 +29,18 @@ def filter(vel_set):
     # x=[[i] for i in x]
     # y=[[i] for i in y]
 
-
+    # print observations.shape
     # Initialize the Kalman Filter
-    data = load_robot()
+    # data = load_robot()
     kf = KalmanFilter(
-        np.eye(2), #data.transition_matrix (dim,dim)
-        np.eye(2), #data.observation_matrix,(2,dim)
+        np.array([[1.2,0.1],[0.1,1.2]]), #data.transition_matrix (dim,dim)
+        observations[-2:,:], #data.observation_matrix,(2,dim)
         np.eye(2), #data.initial_transition_covariance,(dim,dim)
         np.transpose(np.eye(2)), #data.initial_observation_covariance,[(0,val),(val,0)]
-        observations, #data.transition_offsets,(500,dim)
-        np.array([1,1]), #data.observation_offset,(2)
-        np.ones(2), #data.initial_state_mean,(dim)
-        np.eye(2), #data.initial_state_covariance,(dim,dim)
+        np.ones((LAST_FRAMES,2))*0.1, #data.transition_offsets,(500,dim)
+        np.array([1,-   1]), #data.observation_offset,(2)
+        np.zeros(2), #data.initial_state_mean,(dim)
+        np.array([[1,0.1],[-0.1,1]]), #data.initial_state_covariance,(dim,dim)
         random_state=0
     )
 
@@ -83,14 +88,16 @@ def filter(vel_set):
 
     return filtered_state_means
 
-
+vel1=[]
+vel_rect=[]
 
 def rectify(dict_here):
+    global count1
     try:
         dict_now = copy(dict_old)
         dict_now.append(dict_here)
         vel_set = []
-        for i in xrange(100):
+        for i in xrange(LAST_FRAMES):
             try:
                 delta_t = dict_now[-i]["time_now"] - dict_old[-(i+1)]["time_now"]
                 delta_x = dict_now[-i]["x"] - dict_now[-(i+1)]["x"]
@@ -99,20 +106,33 @@ def rectify(dict_here):
                 vel_y = delta_y/delta_t
                 vel_set.append([vel_x, vel_y])
             except: break
+        # print("vs: ",vel_set)
         rectified_set = filter(vel_set)
         dict_updated = copy(dict_here)
-        dict_updated["x"] += rectified_set[-1][0] * (dict_now[-1]["time_now"] - dict_old[-2]["time_now"])
-        dict_updated["y"] += rectified_set[-1][1] * dict_now[-1]["time_now"] - dict_old[-2]["time_now"]
+        # print ("rs:",rectified_set[-1][0])
+        dict_updated["x"] += rectified_set[-1][0] * (dict_now[-1]["time_now"] - dict_now[-2]["time_now"])
+        dict_updated["y"] += rectified_set[-1][1] * (dict_now[-1]["time_now"] - dict_now[-2]["time_now"])
+        vel_rect.append([count1,sqrt(pow(rectified_set[-1][0],2)+pow(rectified_set[-1][1],2))])
+        count1+=1
         return dict_updated
     except:
         return copy(dict_here)
 
+arr1=[]
+arr_rect=[]
+count2=0
+
 def callback(data):
-    # print "I heard: "+str(data.homePos[3].x)
+    global count2
+    print "\n\nI heard: "+str(data.ballPos.x)+" "+str(data.ballPos.y)
     dict_now = {"x": data.ballPos.x, "y":data.ballPos.y, "time_now": time.time()}
+    arr1.append([data.ballPos.x,data.ballPos.y])
+    vel1.append([count2,sqrt(pow(data.ballVel.x,2)+pow(data.ballVel.y,2))])
+    count2+=1
 
     dict_updated = rectify(dict_now)
     print dict_updated
+    arr_rect.append([dict_updated["x"],dict_updated["y"]])
 
     dict_old.append(dict_updated)
     # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
@@ -126,10 +146,17 @@ def listener():
     # run simultaneously.
     rospy.init_node('filter', anonymous=True)
 
-    rospy.Subscriber("belief_state", BeliefState, callback)
+    rospy.Subscriber("vision", BeliefState, callback)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
     listener()
+    vel_rect = np.array(vel_rect)
+    vel1 = np.array(vel1)
+    # print ("vel1:",(vel1))
+    # print ("vel_rect:",vel_rect)
+    pl.plot(vel_rect[:,0],vel_rect[:,1],color='b')
+    pl.plot(vel1[:,0],vel1[:,1],color='r')
+    pl.show()
