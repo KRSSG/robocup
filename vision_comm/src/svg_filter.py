a808e227d23ecac503439ef87502fb4d4ac25a09
americast <sayan.sinha@iitkgp.ac.in>
2017-12-10 20:52:36 +0530

    Pub to belief state

--- vision_comm/src/svg_filter.py
+++ vision_comm/src/svg_filter.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pylab as pl
+import sys, os
 
 import scipy, scipy.signal as sps
 
@@ -25,7 +26,10 @@ vel_rect=[]
 vel_rect_x=[]
 vel_rect_y=[]
 
-def rectify(dict_here, vel_dict_here):
+home_kub_old=[[],[],[],[],[],[]]
+away_kub_old=[[],[],[],[],[],[]]
+
+def rectify(dict_here, vel_dict_here, dict_old, vel_dict_old):
     global count1
     try:
         dict_now = copy(dict_old[-LAST_FRAMES:])
@@ -36,6 +40,7 @@ def rectify(dict_here, vel_dict_here):
         vel_set_y = []
         pos_set_x = []
         pos_set_y = []
+        # print("dict now:", dict_now)
         for i in xrange(len(dict_now)):
             try:
                 pos_set_x.append(dict_now[i]["x"])
@@ -43,9 +48,15 @@ def rectify(dict_here, vel_dict_here):
                 pos_set_y.append(dict_now[i]["y"])
                 vel_set_y.append(vel_dict_now[i]["y"])
             except Exception as e: 
+                print "here 1"
                 print e
+                exc_type, exc_obj, exc_tb = sys.exc_info()
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print(exc_type, fname, exc_tb.tb_lineno)
                 break
                 # print("vs: ",vel_set)
+
+        # print "line no: "+str(len(pos_set_x))
         rectified_set_x = sps.savgol_filter(np.array(pos_set_x),LAST_FRAMES,3)
         rectified_vel_set_x = sps.savgol_filter(np.array(vel_set_x),LAST_FRAMES,3)
         rectified_set_y = sps.savgol_filter(np.array(pos_set_y),LAST_FRAMES,3)
@@ -74,7 +85,11 @@ def rectify(dict_here, vel_dict_here):
         count1+=1
         return dict_updated, vel_dict_updated
     except Exception as e:
+        print "here 2"
         print e
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print(exc_type, fname, exc_tb.tb_lineno)
         return copy(dict_here), copy(vel_dict_here)
 
 arr1=[]
@@ -82,10 +97,12 @@ arr_rect=[]
 count2=0
 
 def callback(data):
+
+    here = copy(data)
     global count2
     print "\n\nI heard: "+str(data.ballPos.x)+" "+str(data.ballPos.y)
-    dict_now = {"x": data.ballPos.x, "y":data.ballPos.y, "time_now": time.time()}
-    vel_dict_now = {"x": data.ballVel.x, "y":data.ballVel.y, "time_now": time.time()}
+    dict_now = {"x": data.ballPos.x, "y":data.ballPos.y}
+    vel_dict_now = {"x": data.ballVel.x, "y":data.ballVel.y}
     arr1.append([data.ballPos.x,data.ballPos.y])
     vel1.append([count2,sqrt(pow(data.ballVel.x,2)+pow(data.ballVel.y,2))])
     vel1_x.append([count2,data.ballVel.x])
@@ -93,13 +110,59 @@ def callback(data):
 
     count2+=1
 
-    dict_updated, vel_dict_updated = rectify(dict_now, vel_dict_now)
-    print dict_updated
+    dict_updated, vel_dict_updated = rectify(dict_now, vel_dict_now, dict_old, vel_dict_old)
+    print ("ball pos: ",dict_updated)
+    print ("ball vel: ",vel_dict_updated)
     arr_rect.append([dict_updated["x"],dict_updated["y"]])
 
     dict_old.append(dict_updated)
     vel_dict_old.append(vel_dict_updated)
-    # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+
+    here.ballPos.x, here.ballPos.y, here.ballVel.x, here.ballVel.y = dict_updated["x"],dict_updated["y"],\
+                                                                    vel_dict_updated["x"], vel_dict_updated["y"]
+
+    for i in xrange(6):
+        home_kub = {"x": data.homePos[i].x, "y":data.homePos[i].y}
+        away_kub = {"x": data.awayPos[i].x, "y":data.awayPos[i].y}
+
+        home_dict_updated, away_dict_updated = rectify(home_kub, away_kub, home_kub_old[i], away_kub_old[i])
+
+        try: home_kub_old[i].append(home_dict_updated)
+        except: 
+                home_kub_old.append([])
+                home_kub_old[i].append(home_dict_updated)
+
+
+        try: away_kub_old[i].append(away_dict_updated)
+        except:
+                away_kub_old.append([])
+                away_kub_old[i].append(away_dict_updated)
+
+
+        here.homePos[i].x = home_dict_updated["x"]
+        here.homePos[i].y = home_dict_updated["y"]
+        
+        here.awayPos[i].x = away_dict_updated["x"]
+        here.awayPos[i].y = away_dict_updated["y"]
+
+        print ("home pos "+str(i),here.homePos[i])
+        print ("away pos "+str(i),here.awayPos[i])
+
+        if len(home_kub_old[i])>LAST_FRAMES:
+            home_kub_old[-LAST_FRAMES:]
+
+        if len(away_kub_old[i])>LAST_FRAMES:
+            away_kub_old[-LAST_FRAMES:]
+
+    pub = rospy.Publisher('belief_state', BeliefState, queue_size=10)
+
+    # rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)
+    pub.publish(here)
+    print "published"
+    # rate.sleep()
+
+        # rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
 def listener():
 
@@ -144,4 +207,4 @@ if __name__ == '__main__':
     pl.scatter(arr1[:,0],arr1[:,1],color='r')
     pl.scatter(arr_rect[:,0],arr_rect[:,1],color='b')
     pl.title('pos')
-    pl.show()
+    # pl.show()
