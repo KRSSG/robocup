8eda6be19530ef70e997db6b3c180a90f7e98689
Mehul Kumar Nirala <MuLx10@users.noreply.github.com>
2017-12-21 18:00:34 +0530

    Update math_functions.py

--- utils/math_functions.py
+++ utils/math_functions.py
@@ -41,3 +41,25 @@ def dist(point1,point2):
 
 def angle_diff(point1,point2):
   return math.atan2(point2.y-point1.y,point2.x-point1.x)
+
+
+def angle_at_vextex(P1,P2,P3):
+    a     = P2.dist(P3)
+    b     = P1.dist(P3)
+    c     = P1.dist(P2)
+    value = (b**2 + c**2 - a**2)/(2*b*c)
+    theta = math.acos(value)
+    return math.atan2(math.tan(theta),1.0)
+
+def area_of_triangle(p1,p2,p3):
+    return math.abs(p1.x*(p2.y-p3.y) + p2.x*(p3.y-p1.y) + p3.x*(p1.y-p2.y))/2.0
+
+def point_in_triangle(t, P):
+    P1, P2, P3 = t[0], t[1], t[2]
+
+    a  = area_of_triangle(P1,P2,P3)
+    a1 = area_of_triangle(P,P2,P3)
+    a2 = area_of_triangle(P1,P,P3)
+    a3 = area_of_triangle(P1,P2,P)
+
+    return a == a1+a2+a3
