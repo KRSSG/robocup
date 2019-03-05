fd2af4a29ca726c389794f418fb62bd887e05131
Manjunath Bhat <manjunathbhat9920@gmail.com>
2019-03-05 23:38:54 +0530

    ompl_planner launch file called in ssl.sh

--- kgpkubs_launch/scripts/ssl.sh
+++ kgpkubs_launch/scripts/ssl.sh
@@ -30,6 +30,7 @@ launcher    "core"         "roscore"
 pr_launcher "vision"    "vision.launch"
 pr_launcher "belief_state"      "belief_state.launch"
 pr_launcher "grsim_comm"    "grsim_comm.launch"
+pr_launcher "path_planner_ompl" "ompl_planner.launch"
 
 screen -S "GUI" -dm python run_gui.py
 screen -S "bs_memcache" -dm python bs.py
