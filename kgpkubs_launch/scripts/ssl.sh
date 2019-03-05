8c4a8be792b3b1f446be717963900054cc66242c
Saurabh Agarwal <22497853+saurabhkgp21@users.noreply.github.com>
2019-03-06 00:39:31 +0530

    remove rosrun ompl_planner command

--- kgpkubs_launch/scripts/ssl.sh
+++ kgpkubs_launch/scripts/ssl.sh
@@ -34,7 +34,6 @@ pr_launcher "path_planner_ompl" "ompl_planner.launch"
 
 screen -S "GUI" -dm python run_gui.py
 screen -S "bs_memcache" -dm python bs.py
-rosrun ompl_planner listener_ompl
 
 # python vision_comm/src/svg_filter.py & >/dev/null
 # pr_launcher "play_node" "play.launch"
