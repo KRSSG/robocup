2864f85dcbd6bd143e8cb8bd0215c52ac60a7174
Manjunath Bhat <manjunathbhat9920@gmail.com>
2019-03-05 22:20:19 +0530

    Update ssl.sh to run everything at once.

--- kgpkubs_launch/scripts/ssl.sh
+++ kgpkubs_launch/scripts/ssl.sh
@@ -19,19 +19,21 @@ function pr_launcher {
 }
 
 ps cax | grep project > /dev/null
-# if [ $? -eq 0 ]; then
-#   echo "grSim already running."
-# else
-#   echo "launching grSim."
-#   launcher "grSim" "rosrun grSim project"
-# fi
+if [ $? -eq 0 ]; then
+    echo "grSim already running."
+else
+    echo "launching grSim."
+    launcher "grSim" "rosrun grSim project"
+fi
 
 launcher    "core"         "roscore"
 pr_launcher "vision"    "vision.launch"
 pr_launcher "belief_state"      "belief_state.launch"
 pr_launcher "grsim_comm"    "grsim_comm.launch"
 
-
+screen -S "GUI" -dm python run_gui.py
+screen -S "bs_memcache" -dm python bs.py
+rosrun ompl_planner listener_ompl
 # python vision_comm/src/svg_filter.py & >/dev/null
 # rosrun ompl_planner listener_ompl 
 # python bs.py
