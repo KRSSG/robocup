ca57424b73d523ada130463e43c1ae7ad5db9486
Saurabh Agarwal <shourabh.agarwal@gmail.com>
2019-03-10 21:10:36 +0530

    new readme and script

--- kgpkubs_launch/scripts/ssl.sh
+++ kgpkubs_launch/scripts/ssl.sh
@@ -1,48 +1,3 @@
-# #!/bin/bash
-# # Start SSL functionality in screen sessions
-# # to kill all sessions: killall -15 screen
-# killall screen
-
-
-# DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# function launcher {
-#     if screen -ls | grep -q $1; then
-#         echo "WARNING! $1 is already running!"
-#     else
-#         screen -S $1 -d -m bash
-#         screen -S $1 -p 0 -X stuff "source $DIR/../../../devel/setup.bash; $2$(printf \\r)"
-#     fi
-# }
-
-# function pr_launcher {
-#     launcher $1 "roslaunch kgpkubs_launch $2 --wait"
-# }
-
-# ps cax | grep project > /dev/null
-# if [ $? -eq 0 ]; then
-#   echo "grSim already running."
-# else
-#   echo "launching grSim."
-#   launcher "grSim" "rosrun grSim project"
-# fi
-
-# launcher    "core"         "roscore"
-# pr_launcher "vision"    "vision.launch"
-# pr_launcher "belief_state"      "belief_state.launch"
-# pr_launcher "grsim_comm"    "grsim_comm.launch"
-
-
-# # python vision_comm/src/svg_filter.py & >/dev/null
-# # rosrun ompl_planner listener_ompl 
-# # python bs.py
-
-# # pr_launcher "robot"         "robot.launch"
-# #pr_launcher "bot_comm"         "bot_comm.launch"
-# #pr_launcher "test_play_node" "test_play.launch"
-# # pr_launcher "play_node" "play.launch"
-# # pr_launcher "rj_robot"         "rj_robot.launch"
-
-# # rosrun grsim_comm debug_test
 #!/bin/bash
 # Start SSL functionality in screen sessions
 # to kill all sessions: killall -15 screen
@@ -82,4 +37,3 @@ screen -S "bs_memcache" -dm python bs.py
 
 # python vision_comm/src/svg_filter.py & >/dev/null
 # pr_launcher "play_node" "play.launch"
-
