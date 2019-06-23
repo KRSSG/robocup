bf0d1ca09e9668626a35a88da8e2f808e6a2d280
kushal2000 <kushal.k@iitkgp.ac.in>
2019-06-24 02:23:17 +0530

    Simplified formula

--- utils/math_functions.py
+++ utils/math_functions.py
@@ -168,10 +168,7 @@ class Line(Structure):
 		return Vector2D(math.cos(angle), math.sin(angle))
 	
 	def nearest_point_on_line(self,point):
-		if math.cos(self.angle) == 0:
-			point = Vector2D(point.x,self.point.y)
-			return point
-		t=((point.y-self.point.y)*math.tan(self.angle)+point.x-self.point.x)*(math.cos(self.angle))
+		t=(point.y-self.point.y)*math.sin(self.angle)+(point.x-self.point.x)*(math.cos(self.angle))
 		x1=self.point.x+math.cos(self.angle)*t
 		y1=self.point.y+math.sin(self.angle)*t
 		point=Vector2D(x1,y1)
