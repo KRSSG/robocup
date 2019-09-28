47074780d073dce6d335c1ec83f911c4b9e1a62f
Parth Mall <parthmall@iitkgp.ac.in>
2019-09-28 21:45:03 +0530

    Better graphs

--- vision_comm/src/kalman_filter.py
+++ vision_comm/src/kalman_filter.py
@@ -12,6 +12,7 @@ from krssg_ssl_msgs.msg import BeliefState, SSL_DetectionFrame, SSL_DetectionBal
 import time
 from copy import copy
 from math import *
+import time
 
 
 dict_old = []
@@ -29,10 +30,10 @@ def makefilter():
     kf.H = np.array([[1, 0, 0, 0],
                      [0, 0, 1, 0]])
     kf.R = np.eye(2) * 0.1**2
-    q = Q_discrete_white_noise(dim=2, dt=dt, var=0.5**2)
+    q = Q_discrete_white_noise(dim=2, dt=dt, var=1.5**2)
     kf.Q = block_diag(q, q)
     kf.x = np.array([0, 0, 0, 0])
-    kf.P = np.eye(4) * 100
+    kf.P = np.eye(4) * 200
 
 def updateFilter(dt):
     global kf
@@ -63,6 +64,15 @@ count2=0
 def callback(data):
     global count2
     try:
+        # i=0
+        # for i in range(len(data.robots_blue)):
+        #     if data.robots_blue[i].robot_id == 0:
+        #         break
+        # print "\n\nI heard: "+str(data.robots_blue[i].x)+" "+str(data.robots_blue[i].y)
+        # dict_now = {"x": data.robots_blue[i].x, "y":data.robots_blue[i].y, "time_now": time.time()}
+        # arr1.append([data.robots_blue[i].x,data.robots_blue[i].y])
+        # if count2==0:
+        #     kf.x = np.array([data.robots_blue[i].x, 0, data.robots_blue[i].y, 0])
         print "\n\nI heard: "+str(data.balls[0].x)+" "+str(data.balls[0].y)
         dict_now = {"x": data.balls[0].x, "y":data.balls[0].y, "time_now": time.time()}
         arr1.append([data.balls[0].x,data.balls[0].y])
@@ -70,7 +80,10 @@ def callback(data):
             kf.x = np.array([data.balls[0].x, 0, data.balls[0].y, 0])
         count2+=1
         dict_old.append(dict_now)
+        start = time.time()
         pos, vel = filter(dict_now)
+        end = time.time()
+        print(end-start)
         print count2
         arr_rect.append(pos)
         vel_rect.append(vel)
@@ -101,23 +114,30 @@ if __name__ == '__main__':
     arr_rect = np.array(arr_rect)
     arr1 = np.array(arr1)
     # plotting the results
-    plt.plot(np.arange(0, len(vel_rect)),vel_rect[:,0])
+    plt.figure()
+    plt.plot(np.arange(0, len(vel_rect)),vel_rect[:,0], color = 'g', label = 'filter')
     plt.xlabel('number of measurments')
     plt.ylabel('x-velocity')
     plt.title('vel_x')
+    plt.legend()
     plt.figure()
-    plt.plot(np.arange(0, len(vel_rect)),vel_rect[:,1])
+    plt.plot(np.arange(0, len(vel_rect)),vel_rect[:,1], color = 'g', label = 'filter')
     plt.xlabel('number of measurments')
     plt.ylabel('y-velocity')
     plt.title('vel_y')
+    plt.legend()
     plt.figure()
-    plt.plot(np.arange(0, len(arr_rect)),arr_rect[:,0])
+    plt.plot(np.arange(0, len(arr_rect)),arr_rect[:,0], ls='--', color = 'k', label = 'filter')
+    plt.plot(np.arange(0, len(arr1)),arr1[:,0], color = 'r', label = 'vision')
     plt.xlabel('number of measurments')
     plt.ylabel('x')
     plt.title('pos_x')
+    plt.legend()
     plt.figure()
-    plt.plot(np.arange(0, len(arr_rect)),arr_rect[:,1])
+    plt.plot(np.arange(0, len(arr_rect)),arr_rect[:,1], ls='--', color = 'k', label = 'filter')
+    plt.plot(np.arange(0, len(arr1)),arr1[:,1], color= 'r', label = 'vision')
     plt.xlabel('number of measurments')
     plt.ylabel('y')
     plt.title('pos_y')
+    plt.legend()
     plt.show()
