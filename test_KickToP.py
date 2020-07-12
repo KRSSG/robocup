818fc202ed0f9fe886905540264a01755155b053
SambhawDrag <vampiresambhaw@gmail.com>
2020-07-12 20:41:42 +0530

    Changes in Kick-To-Point Role, Changes in Driver File test_KickToP.py and added new Driver File for KickToPoint KickP.py

--- test_KickToP.py
+++ test_KickToP.py
@@ -1,8 +1,10 @@
-print "In test GOToPoint"
+print "In test GoToPoint"
 from kubs import kubs, cmd_node
 from velocity.run import *
-import rospy,sys
+import rospy,sys,os
 import math
+import time
+from role import GoToBall, GoToPoint, KickToPointP
 from krssg_ssl_msgs.msg import point_2d
 from krssg_ssl_msgs.msg import BeliefState
 from krssg_ssl_msgs.msg import gr_Commands
@@ -11,19 +13,27 @@ from krssg_ssl_msgs.msg import point_SF
 from utils.math_functions import *
 from utils.config import *
 from krssg_ssl_msgs.srv import bsServer
-import sys
+import subprocess
 
 BOT_ID = int(sys.argv[1])
-print "bot_id received",BOT_ID
+x=int(sys.argv[2])
+y=int(sys.argv[3])
+
+target=Vector2D(x,y)
+
+print "BOT_ID Received",BOT_ID
 pub = rospy.Publisher('/grsim_data', gr_Commands, queue_size=1000)
 
 GOAL_POINT = point_2d()
 GOAL_POINT.x = 1000
 GOAL_POINT.y = 1200
+READY_TO_KICK = False
 REPLANNED = 0
 homePos = None
 awayPos = None
 state = None
+
+
 rospy.wait_for_service('bsServer',)
 getState = rospy.ServiceProxy('bsServer',bsServer)
 try:
@@ -32,7 +42,10 @@ except rospy.ServiceException, e:
 	print e
 if state:
 	BState = state.stateB
+
 kub = kubs.kubs(BOT_ID, BState, pub)
+
+
 st = None 
 tnow = None 
 planned = False
@@ -40,7 +53,11 @@ ramp_upt = 5.0
 ramp_dnt = 0.0
 ramp_rampt = 0.0
 case = -1
-mvw = 0.0
+
+mvw=1.0
+mvx=1.0
+mvy=1.0
+
 
 def reset():
 	global start_time
@@ -53,130 +70,151 @@ def GUI_Callback(data):
 	print BOT_ID, "_____________________________"
 	kub = kubs.kubs(BOT_ID, BState, pub)
 
+
+
+
+def function(id_,state):
+	kub = kubs.kubs(id_,state,pub)
+	kub.update_state(state)
+	print(kub.kubs_id) 
+	g_fsm = KickToPointP.KickToPoint(target)
+	# g_fsm = GoToPoint.GoToPoint()
+	g_fsm.add_kub(kub)
+	# g_fsm.add_point(point=kub.state.ballPos,orient=normalize_angle(pi+atan2(state.ballPos.y,state.ballPos.x-3000)))
+	g_fsm.add_theta(theta=normalize_angle(atan2(target.y - state.ballPos.y,target.x - state.ballPos.x)))
+	g_fsm.get_pos_as_vec2d(target)
+    #g_fsm.as_graphviz()
+	#g_fsm.write_diagram_png()
+	print('something before spin')
+	g_fsm.spin()
+	# 
+
+
+
+
 def kp_callback(data):
-	global st, planned, ramp_rampt, ramp_dnt, ramp_upt, case, mvw, tnow
-	target = Vector2D(0,0)
+	global st, planned, ramp_rampt, ramp_dnt, ramp_upt, case, tnow
+	
 	ballpos = data.ballPos
 	theta = angle_diff(target, ballpos)
-	go_at = getPointToGo(ballpos, theta)
+	go_at = getPointBehindTheBall(ballpos, theta, 2)
+
 	print(radian_2_deg(theta))
 	print(go_at.x)
 	print(go_at.y)
+	
 	t = rospy.Time.now()
 	t = t.secs + 1.0*t.nsecs/pow(10,9)
 	print(" t - start = ",t-start_time)
-	[vx, vy, vw, REPLANNED] = Get_Vel(start_time, t, BOT_ID, go_at, data.homePos, data.awayPos, True)
+	[vx, vy, vw, REPLANNED] = Get_Vel(start_time, t, BOT_ID, go_at, data.homePos, data.awayPos, True, 1.2)
 		#vx, vy, vw, replanned
-	print("-------------------REPLANNED = ",REPLANNED)
+	print("-------------------  REPLANNED = ",REPLANNED)
 	if(REPLANNED):
-		reset()
+    		reset()
+	
 	print("vx = ",vx)
 	print("vy = ",vy)
+	
+	mvx=vx
+	mvy=vy
+
 	# print("kubs_id = ",kub.kubs_id)
+
 	curPos = Vector2D(int(data.homePos[BOT_ID].x),int(data.homePos[BOT_ID].y))
 	#if vicinity_points(go_at, curPos, 4) == False:
 	try:	
 		kub.move(vx, vy)
 		print(vw)
-		print("homePos")
+		print("#Test Statement ---- homePos")
 		kub.turn(vw)
 		kub.execute()
 	except Exception as e:
 		print("In except",e)
 		pass
 	print(dist(go_at, curPos))
-	# if planned == False:
-	# 	theta_lft = abs(normalize_angle(normalize_angle(data.homePos[BOT_ID].theta)-theta))
-	# 	print(theta_lft)
-	# 	if theta_lft >= 5.0*MAX_BOT_OMEGA:
-	# 		case = 1
-	# 		ramp_upt = 5.0
-	# 		ramp_dnt = 5.0
-	# 		ramp_rampt = (theta_lft-5.0*MAX_BOT_OMEGA)*1.0/MAX_BOT_OMEGA
-	# 	elif theta_lft >= 2.5*MAX_BOT_OMEGA:
-	# 		case = 2
-	# 		ramp_upt = 0.0
-	# 		ramp_dnt = 5.0
-	# 		ramp_rampt = (theta_lft-2.5*MAX_BOT_OMEGA)*1.0/MAX_BOT_OMEGA
-	# 	else:
-	# 		case = 3
-	# 		ramp_rampt = 0.0
-	# 		ramp_dnt = sqrt(2.0*theta_lft/MAX_BOT_OMEGA_ACC)*1.0
-	# 		mvw = sqrt(2.0*theta_lft*MAX_BOT_OMEGA_ACC)*1.0
-	# 		ramp_upt = 0.0
-	# 	planned = True
-	# print(case)
-	# print(ramp_rampt)
-	# print(ramp_upt)
-	# print(ramp_dnt)
-	# print(mvw)
+	
 	if vx == 0 and vy == 0:
-		print("TURNING")
+    	
+
 		print(data.homePos[BOT_ID].theta)
 		print(theta)
 		totalAngle = theta
 		MAX_w = (MAX_BOT_OMEGA+MIN_BOT_OMEGA)/1.2
 		# theta_left = float(homePos[kub_id].theta-totalAngle)
 		theta_lft = normalize_angle(normalize_angle(data.homePos[BOT_ID].theta)-totalAngle)*-1.0+3.1412
-		vw = (theta_lft/2*math.pi)*MAX_w
-		# print "totalAngle",radian_2_deg(totalAngle)
-		# print "theta_left ",radian_2_deg(theta_lft),theta_lft
-		# print "homePos theta ",radian_2_deg(normalize_angle(homePos[kub_id].theta))
-		# print "omega ",vw
-		if abs(vw)<1*MIN_BOT_OMEGA:
+		
+		if abs(theta_lft)<ROTATION_FACTOR/100:
+			vw = 0.0
+			READY_TO_KICK=True
+		else:
+			READY_TO_KICK=False
+			vw = (theta_lft/2*math.pi)*MAX_w
+
+		if abs(vw)<1*MIN_BOT_OMEGA and READY_TO_KICK==False:
 			vw = 1*MIN_BOT_OMEGA*(1 if vw>0 else -1)
 
-		if abs(theta_lft)<ROTATION_FACTOR/2:
-			vw = 0.0
 
-		print "Omega return",vw
+		if vw!=0:
+			print("TURNING")
+		else:
+			print("DONE")
+			
+		mvw=vw
+
+		
+
+		print "Omega Return",vw
+		print "TRY: ",mvw
+		print READY_TO_KICK
+		print "___________________________________________________________________"
 		kub.reset()
 		kub.turn(vw)
 		kub.execute()
-		# if st == None:
-		# 	st = rospy.Time.now()
-		# 	st = 1.0*st.secs + 1.0*st.nsecs/pow(10,9)
-		# tnow = rospy.Time.now()
-		# tnow = 1.0*tnow.secs + 1.0*tnow.nsecs/pow(10,9)
-		# diff = tnow-st
-		# if case == 1:
-		# 	if diff <= 5.0:
-		# 		vw = (tnow-st)*MAX_BOT_OMEGA_ACC
-		# 	elif diff > 5.0 and diff <= 5.0+ramp_rampt:
-		# 		vw = MAX_BOT_OMEGA
-		# 	else:
-		# 		tim = 10.0+ramp_rampt-diff
-		# 		vw = tim*MAX_BOT_OMEGA_ACC
-		# elif case == 2:
-		# 	if diff <= ramp_rampt:
-		# 		vw = MAX_BOT_OMEGA
-		# 	else :
-		# 		tim = 5.0+ramp_rampt-diff
-		# 		vw = tim*MAX_BOT_OMEGA_ACC
-		# else :
-		# 	vw = mvw - MAX_BOT_OMEGA_ACC*diff 
-		# if normalize_angle(homePos[BOT_ID].theta) < theta:
-		# 	vw = -1.0*vw
-		# print(vw)
-		# kub.reset()
-		# kub.turn(vw)
-		# kub.execute()
+
+		if READY_TO_KICK  :
+    			print "\nAngle Remaining : ",theta_lft
+    			print "      ____________REACHED BALL, READY TO KICK____________\n"
+    			rospy.signal_shutdown("=======ALIGNED======")
+
 
 
 if __name__ == "__main__":
 	global start_time, st, planned, case, mvw
 	case = -1
-	mvw = 0.0
 	st = None
 	planned = False
 	print("here")
+
+	#Begin the process: Driver, START MOVING
+
 	rospy.init_node('node_new',anonymous=False)
 	start_time = rospy.Time.now()
 	start_time = 1.0*start_time.secs + 1.0*start_time.nsecs/pow(10,9)
+
+
 	pub = rospy.Publisher('/grsim_data', gr_Commands, queue_size=1000)
 	print('testing...')
-	rospy.Subscriber('/belief_state', BeliefState, kp_callback, queue_size=1000)
-	rospy.Subscriber('/gui_params', point_SF, GUI_Callback, queue_size = 1000)
+	sub1 = rospy.Subscriber('/belief_state', BeliefState, kp_callback, queue_size=1000)
+	
+	sub2 = rospy.Subscriber('/gui_params', point_SF, GUI_Callback, queue_size = 1000)
+
+	print("Ho gya!!")
 	rospy.spin()
+	
+	sub1.unregister()
+	sub2.unregister()
+	print "AB MAIN KARUNGAAA -------- KICKKK !!!"
+
+command="python KickP.py %s %s %s"%(BOT_ID,target.x,target.y)
+print(command)
+os.system(command)
+	
 
 
+#cmd = "date"
+
+#returns output as byte string
+#returned_output = subprocess.check_output(cmd)
+
+#using decode() function to convert byte string to string
+#print('Current date is:', returned_output.decode("utf-8"))
